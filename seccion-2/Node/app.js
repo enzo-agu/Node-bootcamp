8e25da60e2e674c5f5e8ca4ec12ddc56ea6a5d96
enzo agu <enzo.gustavo.agu@gmail.com>
2023-12-05 12:55:42 -0300

    sección-2

---
+++ seccion-2/Node/app.js
@@ -0,0 +1,28 @@
+// let nombre = "enzo";
+
+// const saludar = (nombre) => {
+//   return `saludo ${nombre}`;
+// };
+// console.log(saludar(nombre));
+
+// ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+
+// console.log("inicio de programa");
+
+// setTimeout(() => {
+//   console.log("primero");
+// }, 3000);
+
+// setTimeout(() => {
+//   console.log("segundo");
+// }, 0);
+
+// setTimeout(() => {
+//   console.log("tercero");
+// }, 0);
+
+// console.log("fin de programa");
+
+
+// ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+
